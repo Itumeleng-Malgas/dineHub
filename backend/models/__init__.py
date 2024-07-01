e3bfa65dcc506b8cddec91134624466e914f8e85
Matrix30 <matrixboy30@gmail.com>
2024-07-01 21:25:24 +0100

    add test folder structure

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,13 +3,14 @@
 
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
-from os import environ
+from os import getenv
 
-storage_type = environ('DINEHUB_TYPE_STORAGE', None)
+storage_type = getenv('DINEHUB_TYPE_STORAGE', None)
 if storage_type:
     if storage_type == 'fs':
         storage = FileStorage()
+        storage.reload()
     else:
         storage = DBStorage()
+        storage.reload()
 
-storage.reload()
